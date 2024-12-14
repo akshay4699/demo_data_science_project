359f521b8e34c3d3041f7a7fcd0f991519ead227
Akshay Kassa <akshukassa234@gmail.com>
2024-12-14 13:56:11 +0530

    update folder name from Data Science Project to datascience

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
-project_name = "Data Science Project 1"
+project_name = "datascience"
 
 list_of_files = {
     ".github/workflow/.gitkeep",
